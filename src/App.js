4ae69ef9d19a78ceb5c91bf88749077b336dadb7
Analog_guy <pooyan.cwz@gmail.com>
2021-08-10 12:08:34 +0430

    making css changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React,{useState,useEffect} from 'react';
 import axios from 'axios'
+import background from './img/bg.jpg'
 function App() {
   const [ quote, setQuote ] = useState({});
   const [errorMessage, setErrorMessage] = useState("");
@@ -20,7 +21,7 @@ function App() {
     }, []);
   
   return (
-    <div className="app-wrapper">
+    <div className="app-wrapper" style={{ backgroundImage: `url(${background})` }}>
       <div className="box-wrapper">
         <div className="title-wrapper"><h1>Quote generator</h1></div>
         <div className="quote-wrapper"><h3>"{quote.content}"</h3></div>
